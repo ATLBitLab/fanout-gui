58bacbaec07062956dc070749661e7693f3b847a
Jordan Bravo <jordan@bravo.cc>
2024-10-23 16:51:15 -0400

    add nix flake and readme

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "fanout-gui flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            pkg-config
+            gcc # GNU Compiler Collection
+            gnumake # Tool to control the generation of non-source files from sources
+          ];
+        };
+      });
+}
+
+
+
